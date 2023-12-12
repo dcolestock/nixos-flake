2807ac06b04d9435fb19199dd44f2d00f4256ed3
Dan Colestock <dan.colestock@userdelta.com>
2023-12-12 16:07:09 -0600

    added gnome perfection plugin for window focus tweaks

--- home/dconf.nix
+++ home/dconf.nix
@@ -2,7 +2,8 @@
   dconf.settings = {
     "org/gnome/shell" = {
       disabled-extensions = [];
-      enabled-extensions = ["dash-to-panel@jderose9.github.com" "appindicatorsupport@rgcjonas.gmail.com"];
+      enabled-extensions = ["dash-to-panel@jderose9.github.com"  "appindicatorsupport@rgcjonas.gmail.com"  "just-perfection-desktop@just-perfection"];
+
       favorite-apps = ["google-chrome.desktop" "kitty.desktop" "discord.desktop" "steam.desktop" "org.gnome.Nautilus.desktop" "org.keepassxc.KeePassXC.desktop" "obsidian.desktop"];
     };
     "org/gnome/shell/extensions/dash-to-panel" = {
@@ -24,6 +25,8 @@
       clock-format = "12h";
     };
 
+    "org/gnome/shell/extensions/just-perfection".window-demands-attention-focus = true;
+    "org/gnome/shell/extensions/just-perfection".startup-status = 0;
     "org/gnome/desktop/wm/keybindings".panel-run-dialog = ["<Super>r"];
     "org/gnome/desktop/wm/preferences".titlebar-font = "Cantarell Bold 12";
     "org/gtk/settings/file-chooser".clock-format = "12h";
