36dd0aed33264aa8c782dabfdcf9d831d26a3b86
Dan Colestock <dan.colestock@userdelta.com>
2024-01-17 10:27:00 -0600

    marimo python package

--- home/python.nix
+++ home/python.nix
@@ -18,12 +18,14 @@ let
     primecountpy
     sortedcontainers
     ply
+    pygraphviz
 
     # Utilities
     more-itertools
     tqdm
     aocd
     icecream
+    rich
 
     # Network
     beautifulsoup4
@@ -46,7 +48,35 @@ let
     scikit-learn
     opencv4
     pytesseract
+
+    # Marimo
+    (
+      buildPythonPackage rec {
+        pname = "marimo";
+        version = "0.1.76";
+        src = fetchPypi {
+          inherit pname version;
+          sha256 = "sha256-uOKd8s0MDSYMR1gf/MzsUQ5sbVl0FtH0I16kMxi94Yc=";
+        };
+        doCheck = false;
+        format = "pyproject";
+        propagatedBuildInputs = with pkgs.python3Packages; [
+          setuptools-scm
+          click
+          importlib-resources
+          jedi
+          markdown
+          pymdown-extensions
+          pygments
+          tomlkit
+          tornado
+          typing-extensions
+          black
+        ];
+      }
+    )
   ] ++ python-lsp-server.passthru.optional-dependencies.all;
-in {
-home.packages = [(pkgs.python311.withPackages my-python-packages)];
+in
+{
+  home.packages = [ (pkgs.python311.withPackages my-python-packages) ];
 }
