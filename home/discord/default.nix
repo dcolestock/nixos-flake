8d9ad5bdcd1308bb3350fc14b24227b1c5bdfb72
Dan Colestock <dan.colestock@userdelta.com>
2024-07-15 11:40:43 -0500

    moved krisp-patcher to python3.11 because capstone is missing from 3.12

--- home/discord/default.nix
+++ home/discord/default.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: let
   krisp-patcher = pkgs.writers.writePython3Bin "krisp-patcher" {
-    libraries = with pkgs.python3Packages; [capstone pyelftools];
+    libraries = with pkgs.python311Packages; [capstone pyelftools];
     flakeIgnore = [
       "E501" # line too long (82 > 79 characters)
       "F403" # ‘from module import *’ used; unable to detect undefined names
