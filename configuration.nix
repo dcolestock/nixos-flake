c921dd7d0eaca5ba1b8a44fee315413435fd6e25
Dan Colestock <dan.colestock@userdelta.com>
2024-12-13 14:51:01 -0600

    Disabled samba server for cups due to port conflict

--- configuration.nix
+++ configuration.nix
@@ -137,32 +137,32 @@
     defaultShared = true;
     openFirewall = true;
   };
-  services.samba = {
-    enable = true;
-    package = pkgs.sambaFull;
-    openFirewall = true;
-    settings = {
-      "global" = {
-        "load printers" = "yes";
-        "printing" = "cups";
-        "printcap name" = "cups";
-      };
-      "printers" = {
-        "comment" = "All Printers";
-        "path" = "/var/spool/samba";
-        "public" = "yes";
-        "browsable" = "yes";
-        # to allow user 'guest account' to print.
-        "guest ok" = "yes";
-        "writable" = "no";
-        "printable" = "yes";
-        "create mode" = 0700;
-      };
-    };
-  };
-  systemd.tmpfiles.rules = [
-    "d /var/spool/samba 1777 root root -"
-  ];
+  # services.samba = {
+  #   enable = true;
+  #   package = pkgs.sambaFull;
+  #   openFirewall = true;
+  #   settings = {
+  #     "global" = {
+  #       "load printers" = "yes";
+  #       "printing" = "cups";
+  #       "printcap name" = "cups";
+  #     };
+  #     "printers" = {
+  #       "comment" = "All Printers";
+  #       "path" = "/var/spool/samba";
+  #       "public" = "yes";
+  #       "browsable" = "yes";
+  #       # to allow user 'guest account' to print.
+  #       "guest ok" = "yes";
+  #       "writable" = "no";
+  #       "printable" = "yes";
+  #       "create mode" = 0700;
+  #     };
+  #   };
+  # };
+  # systemd.tmpfiles.rules = [
+  #   "d /var/spool/samba 1777 root root -"
+  # ];
 
   hardware = {
     bluetooth = {
