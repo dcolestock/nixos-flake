7c6e7cd10bd0d5b64f16a4840246c6a9483702c2
Dan Colestock <dan.colestock@userdelta.com>
2025-03-31 12:15:16 -0500

    Added hotkey for toggling virtual_lines and also virtual_lines only shows errors

--- home/neovim.nix
+++ home/neovim.nix
@@ -445,10 +445,33 @@ in {
             lspconfig.rust_analyzer.setup({
               capabilities = capabilities,
             })
-            vim.diagnostic.config({ virtual_lines = true })
+            local diag_config1 = {
+              virtual_text = {
+                enabled = true,
+                severity = {
+                  max = vim.diagnostic.severity.WARN,
+                },
+              },
+              virtual_lines = {
+                enabled = true,
+                severity = {
+                  min = vim.diagnostic.severity.ERROR,
+                },
+              },
+            }
+            local diag_config2 = {
+              virtual_text = true,
+              virtual_lines = false,
+            }
+            vim.diagnostic.config(diag_config1)
+            local diag_config_basic = false
             vim.keymap.set("n", "gK", function()
-              local new_config = not vim.diagnostic.config().virtual_lines
-              vim.diagnostic.config({ virtual_lines = new_config })
+              diag_config_basic = not diag_config_basic
+              if diag_config_basic then
+                vim.diagnostic.config(diag_config2)
+              else
+                vim.diagnostic.config(diag_config1)
+              end
             end, { desc = "Toggle diagnostic virtual_lines" })
           '';
       }
