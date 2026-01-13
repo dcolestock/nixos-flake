990cca6364c604937947f4e44363a6333901d362
Dan Colestock <dan.colestock@userdelta.com>
2026-01-13 16:35:09 -0600

    remove kde wayland

--- configuration.nix
+++ configuration.nix
@@ -121,7 +121,7 @@
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
   services.displayManager.defaultSession = "plasma";
-  services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.wayland.enable = false;
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     plasma-browser-integration
